eee782d0236bfd724f26e78c8fdd9ec77a4d775c
weslley <weslleyborges34@gmail.com>
2022-09-04 14:12:59 -0300

    Nome de tabela não pode repitir

--- Criar_tabela/Colunas_tabela.php
+++ Criar_tabela/Colunas_tabela.php
@@ -18,13 +18,29 @@
               $_SESSION['nome_tabela'] = mysqli_escape_string($conect, $_POST['nome_tabela']);
               $_SESSION['numero_colunas'] = mysqli_escape_string($conect, $_POST['numero_colunas']);
 
-              echo $_SESSION['nome_tabela']." and ".$_SESSION['numero_colunas']." <br/>";
+              $tabela_existe = "SHOW TABLES LIKE '".$_SESSION['nome_tabela']."'";
+              $query_tabela_existe = mysqli_query($conect, $tabela_existe);
+              if ($query_tabela_existe) {
+                $row = mysqli_num_rows($query_tabela_existe);
+                if (!$row == 0) {
+                  echo "O nome de tabela já está sendo usado";
+                }else{
+                  echo $_SESSION['nome_tabela']." Possui ".$_SESSION['numero_colunas']." colunas. <br/>";
 
-              for ($i=0; $i < $_SESSION['numero_colunas']; $i++) {
-                echo "<input name='campo$i' type='text' placeholder='Nome do campo' required><br/>";
+                  for ($i=0; $i < $_SESSION['numero_colunas']; $i++) {
+                    echo "<input name='campo$i' type='text' placeholder='Nome do campo' required><br/>";
+                  }
+                  echo "<input type='submit' name='Criar_campos' value='Criar campos'>";
+                  
+                }
+              }else {
+                echo "<br/>ERRO AO VERIFICAR SE TABELA EXISTE<br/>";
               }
+
+
+
           ?>
-    <input type="submit" name="Criar_campos" value="Criar campos">
+
   </form>
 
 </body>
