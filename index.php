ecadaf5909b90ccd5beaa34ccd21b09482ac94b4
Weslley Pereira Borges <weslleyborges34@gmail.com>
2022-09-14 16:40:40 -0300

    Css do index.php criado

--- index.php
+++ index.php
@@ -2,14 +2,23 @@
 <html lang="pt-BR" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
+    <link rel="stylesheet" href="Css/style.css">
   </head>
   <body>
 
-    <a href="/Tabelas/Criar_tabela">Criar tabela</a> <br/>
-    <a href="/Tabelas/Exibir_tabela">Exibir tabela</a> <br/>
-    <a href="/Tabelas/Novo_alterar_tabela">Alterar tabela</a> <br/>
-    <a href="/Tabelas/Apagar_tabela">Apagar tabela</a>
+    <span class="titulos">
+      <h1>Sistema organizacional de tabelas</h1>
+      <h2>Selecione a opção que deseja realizar.</h2>
+    </span>
+
+    <nav aria-label="Primária" class = "opcoes">
+        <a href="/Tabelas/Criar_tabela">Criar tabela</a>
+        <a href="/Tabelas/Exibir_tabela">Exibir tabela</a>
+        <a href="/Tabelas/Novo_alterar_tabela">Alterar tabela</a>
+        <a href="/Tabelas/Apagar_tabela">Apagar tabela</a>
+    </nav>
 
   </body>
 </html>
